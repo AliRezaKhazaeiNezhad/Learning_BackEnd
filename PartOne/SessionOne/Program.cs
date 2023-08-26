bdae04e8f4994459d7d80256be865ded39f36276
Ali Reza <Ali Reza@DESKTOP-AF2LFC9>
2023-08-26 11:04:36 +0330

    جلسه اول

---
+++ PartOne/SessionOne/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessionOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Learn 1
+            //var myData = 1;
+            //var myData2 = 19.75;
+            //var myData3 = true;
+            //var myData4 = "Ali Reza Khazaei Nezhad";
+            //var myData5 = DateTime.Now;
+            //var myData6 = new object();
+
+            //Learn 2
+            //int myNumber = 1;
+            //Console.WriteLine(myNumber);    
+
+            //Learn 3
+            //int numberOne = 10;
+            //int numberTwo = 20; 
+            //int result = numberOne + numberTwo; 
+            //Console.WriteLine(result);  
+
+            //Learn 4
+            //int numberOne = 10;
+            //int numberTwo = 20;
+            //int result = numberOne - numberTwo;
+            //Console.WriteLine(result);
+
+            //Learn 5
+            //int numberOne = 10;
+            //int numberTwo = 20;
+            //int result = numberOne * numberTwo;
+            //Console.WriteLine(result);
+
+            //Learn 6
+            //int numberOne = 10;
+            //int numberTwo = 20;
+            //int result = numberOne / numberTwo;
+            //Console.WriteLine(result);
+
+            //Learn 7
+            //int numberOne = 10;
+            //int numberTwo = 20;
+            //int result = numberOne % numberTwo;
+            //Console.WriteLine(result);
+
+            //Learn 8
+            //int myNumber = 1.45;
+            //Console.WriteLine(myNumber);
+
+            //Learn 9
+            //double price = 49.55;
+            //Console.WriteLine(price);
+
+            //Learn 10
+            //double numberOne = 10;
+            //double numberTwo = 20;
+            //int result = numberOne + numberTwo;
+            //Console.WriteLine(result);
+
+            //Learn 11
+            //double numberOne = 10;
+            //double numberTwo = 20;
+            //int result = numberOne - numberTwo;
+            //Console.WriteLine(result);
+
+            //Learn 12
+            //double numberOne = 10;
+            //double numberTwo = 20;
+            //int result = numberOne * numberTwo;
+            //Console.WriteLine(result);
+
+            //Learn 13
+            //double numberOne = 10;
+            //double numberTwo = 20;
+            //int result = numberOne / numberTwo;
+            //Console.WriteLine(result);
+
+            //Learn 14
+            //double numberOne = 10;
+            //double numberTwo = 20;
+            //int result = numberOne % numberTwo;
+            //Console.WriteLine(result);
+
+            //Learn 15
+            //double price = 49;
+            //Console.WriteLine(price);
+
+            //Learn 16
+            //bool state = false;
+            //Console.WriteLine(state);
+
+            //Learn 17
+            //bool state = true;
+            //Console.WriteLine(state);
+
+            //Learn 18
+            //string firstName = "Ali Reza";
+            //Console.WriteLine(firstName);
+
+            //Learn 19
+            //string firstName = "Ali Reza";
+            //string lastName = "Khazaei Nezhad";
+            //string fullName = firstName + lastName; 
+            //Console.WriteLine(fullName);
+
+            //Learn 20
+            //string firstName = "Ali Reza";
+            //string lastName = "Khazaei Nezhad";
+            //string fullName = firstName + " " + lastName;
+            //Console.WriteLine(fullName);
+
+            //Learn 21
+            //string firstName = "Ali Reza";
+            //string lastName = "Khazaei Nezhad";
+            //string fullName = $"{firstName} {lastName}";
+            //Console.WriteLine(fullName);
+
+            //Learn 22
+            //DateTime dtNow = DateTime.Now;
+            //Console.WriteLine(dtNow);
+
+            //Learn 23
+            //DateTime dtNow = DateTime.Now.AddHours(26);
+            //Console.WriteLine(dtNow);
+
+            //Learn 24
+            //DateTime dtNow = DateTime.Now.AddHours(-26);
+            //Console.WriteLine(dtNow);
+
+            //Learn 25
+            //object product = new object();
+            //Console.WriteLine(product);
+        }
+    }
+}
